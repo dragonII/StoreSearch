cd2c08b1ab69524d5601825d06d7be0ce1ac4458
lwang <wanglong1982@gmail.com>
2014-11-09 22:41:24 +0800

    Initial Commit

---
+++ StoreSearch/PTRXViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTRXViewController.h
+//  StoreSearch
+//
+//  Created by Wang Long on 11/9/14.
+//  Copyright (c) 2014 Wang Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTRXViewController : UIViewController
+
+@end
---
+++ StoreSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoreSearch
+//
+//  Created by Wang Long on 11/9/14.
+//  Copyright (c) 2014 Wang Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PTRXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTRXAppDelegate class]));
+    }
+}
